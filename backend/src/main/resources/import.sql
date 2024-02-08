5362f12c6d1e8eade6b35a14c52d10350cb736c9
MaiconRomario <maiconromario389@hotmail.com>
2024-02-07 23:01:31 -0300

    Course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://upload.wikimedia.org/wikipedia/commons/2/27/Switch-course-book.svg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdPnsLZkhz-Ck97J5oCZon6ZZNDVCcvoPhwCj8L2BThkepSmwBBgIyX-3BydEoNbTXZCk&usqp=CAU'); 
