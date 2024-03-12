65617c18c12e23c9261ccf82ae9b1493e5890013
MaiconRomario <maiconromario389@hotmail.com>
2024-03-12 13:54:45 -0300

    Offer

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -13,4 +13,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://upload.wikimedia.org/wikipedia/commons/2/27/Switch-course-book.svg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdPnsLZkhz-Ck97J5oCZon6ZZNDVCcvoPhwCj8L2BThkepSmwBBgIyX-3BydEoNbTXZCk&usqp=CAU'); 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://upload.wikimedia.org/wikipedia/commons/2/27/Switch-course-book.svg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdPnsLZkhz-Ck97J5oCZon6ZZNDVCcvoPhwCj8L2BThkepSmwBBgIyX-3BydEoNbTXZCk&usqp=CAU');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2024-07-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-07-14T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2024-12-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-12-14T03:00:00Z', 1);
