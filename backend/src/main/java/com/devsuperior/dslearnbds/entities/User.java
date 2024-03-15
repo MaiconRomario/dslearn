b699dfa5bf0ec7008fffe70e6081610932665988
MaiconRomario <maiconromario389@hotmail.com>
2024-03-15 14:55:01 -0300

    Adjustment of some entities

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -39,16 +39,16 @@ public class User implements Serializable {
 	@OneToMany(mappedBy = "user")
 	private List<Notification> notifications = new ArrayList<>();
 	
-	
 	public User() {}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+
+
+	public User(Long id, String name, String email, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
 	}
 
 	public Long getId() {
@@ -85,10 +85,6 @@ public class User implements Serializable {
 
 	public Set<Role> getRoles() {
 		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
 	}
 
 	public List<Notification> getNotifications() {
